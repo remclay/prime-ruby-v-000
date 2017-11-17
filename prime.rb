5680140a0cad5b7b2d4085cf91ca4f7071609f41
Remmy Clay <remmy.lauren@gmail.com>
2017-11-17 05:20:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,34 +2,38 @@
 # Add  code here!
 
 # EVEN AND ODD NUMBERS
-#def prime?(integer)
-#  if integer < 2
+#def prime?(i)
+#  if i < 2
 #    false
 #  else
 #    array = (2..(integer - 1)).to_a    #need brackets?
-#    array.none? {|n| integer % n == 0}
+#    array.none? {|n| i % n == 0}
 #  end
 #end
 
 # REMOVE EVEN NUMBERS
-#def prime?(integer)
-#  if integer < 2 || integer%2 == 0
+#def prime?(i)
+#  if i < 2 || i%2 == 0
 #    false
 #  else
-#    array = (2..(integer-1)).to_a
+#    array = (2..(i-1)).to_a
 #    array.delete_if {|n| n%2 == 0 }
-#    array.none? {|n| integer % n == 0}
+#    array.none? {|n| i % n == 0}
 #  end
 #end
 
 # ANY VS NONE
-#def prime?(integer)
-#    if integer < 2
+#def prime?(i)
+#    if i < 2
 #    false
 #  else
-#    array = (2..(integer - 1)).to_a
+#    array = (2..(i - 1)).to_a
 #    !array.any? do |n|
-#      (integer % n == 0)
+#      (i % n == 0)
 #    end
 #  end
 #end
+
+def prime?(i)
+  i > 1 ? !(2..i/2).any? { |n| i%n == 0 } : false
+end
